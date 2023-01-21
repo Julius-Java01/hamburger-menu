bc368d1615e78b14174337d14d7f388db05b8503
= <emmanueljulius9102016@gmail.com>
2023-01-21 14:34:56 +0100

    Change timeout to 100ms

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ $(document).ready(() => {
     const menuBtn = $('i.fa-solid')
 
     menuBtn.click(() => {
-        menuBtn.toggleClass('open')
+        menuBtn.toggleClass('rotate-half')
+
+        setTimeout(() => {
+            menuBtn.toggleClass('fa-bars') // removes the bar-icon
+            menuBtn.toggleClass('rotate-full fa-arrow-right-long') // rotates 
+        }, 100)
     })
 })
